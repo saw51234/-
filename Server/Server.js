6dec076d24c0c075c86d3fd039b0ee24a247d114
solmin020619 <solmin0619@naver.com>
2025-12-14 05:31:05 +0900

    [Fix] env 수정

--- Server/Server.js
+++ Server/Server.js
@@ -2,10 +2,10 @@ require('dotenv').config();   // .env 읽기
 
 const express = require('express');
 const cors = require('cors');
+
 const db = require('./db/db');       // DB 연결
 const authRoutes = require('./routes/auth');   // auth 라우트 추가
 const gachaRoutes = require('./routes/gacha');
-app.use('/api/gacha', gachaRoutes);
 
 const app = express();
 
@@ -19,6 +19,7 @@ app.get('/ping', (req, res) => {
 
 // 여기서 auth 라우트를 등록하는 것
 app.use('/api/auth', authRoutes);
+app.use('/api/gacha', gachaRoutes);
 
 // 포트 설정
 const PORT = process.env.PORT || 4000;
